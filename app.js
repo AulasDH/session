68011caf3669486eb179ac19aa57070f506be009
tsunodajapa <gabrieltsunoda@gmail.com>
2022-03-23 20:27:09 -0300

    feat: inicio da controller

--- app.js
+++ app.js
@@ -6,10 +6,15 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src', 'views'));
 
+app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (request, response) => {
     response.render('login');
+});
+
+app.post('/login', (request, response) => {
+    console.log(request.body);
 });
 
 app.listen('3000', () => {
