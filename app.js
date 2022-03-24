84086b97080923da83fd6399942f70bd721d95ca
tsunodajapa <gabrieltsunoda@gmail.com>
2022-03-23 21:46:50 -0300

    feat: session

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const session = require('express-session');
 
 const authController = require('./src/controllers/authController');
 
@@ -10,10 +11,25 @@ app.set('views', path.join(__dirname, 'src', 'views'));
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: 'XxdDQo4F5A*btj5Ai5#EWaA!I$'
+}));
 
 app.get('/', authController.show);
 app.post('/login', authController.login);
+app.get('/admin', estaAutorizado, (request, response) => {
+    console.log('Eh Autorizado?', request.session)
+    response.render('admin');
+});
 
 app.listen('3000', () => {
     console.log('Aplicação executada na porta 3000');
 });
+
+function estaAutorizado(request, response, next) {
+    if (request.session.autorizado) {
+        return next();
+    }
+
+    return response.redirect('/');
+}
